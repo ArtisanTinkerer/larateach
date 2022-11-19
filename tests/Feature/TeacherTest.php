9fc1390e8f9fac1e35ca7a40734eac5e7891b221
Mick Byrne <mick.byrne@ospreyfs.com>
2022-11-19 21:21:35 +0000

    getting responses from Wonde

--- tests/Feature/TeacherTest.php
+++ tests/Feature/TeacherTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Http;
+use Wonde\Client;
 
 class TeacherTest extends TestCase
 {
+
     /**
      * A basic test example.
      *
@@ -45,15 +47,55 @@ class TeacherTest extends TestCase
 //-H "Authorization: Bearer 7e76896f9aca62569048c667db292d72dd84f224"
 
 
+    // can auth [x]
 
     /** @test */
     public function can_auth_with_wonde()
     {
         $response = Http::withToken(env('WONDE_TOKEN'))
-            ->get('https://api.wonde.com/v1.0');
+            ->get('https://api.wonde.com/v1.0'); // UK domain
 
         $this->assertTrue($response->ok());
     }
 
+    // look at the sdk[x]
+
+    /** @test */
+    public function can_connect_with_client()
+    {
+        $client = new Client(env('WONDE_TOKEN'));
+        $school = $client->schools->get(env('TEST_SCHOOL_ID'));
+        $this->assertTrue($school->id === env('TEST_SCHOOL_ID') );
+    }
+
+    //sdk has classes index
+    //and teachers index
+
+
+
+    //exception handing[]
+
+    //teacher wants to see who is in class
+    //each day of week
+
+    // could use GET Employees
+    // will return classes
+
+    /** @test */
+    public function can_get_employees()
+    {
+        $client = new Client(env('WONDE_TOKEN'));
+        $school = $client->schools->get(env('TEST_SCHOOL_ID'));
+
+        foreach ($school->employees->all() as $employee) {
+            echo $employee->forename . ' ' . $employee->surname . PHP_EOL;
+        }
+
+        //$this->assertTrue($school->id === env('TEST_SCHOOL_ID') );
+    }
+
+
+
+    //check updated_after []
 
 }
